4d18541d21942a46c349a14290f7e2280f8879cb
Youness CN8VX <cn8vx.ma@gmail.com>
2024-10-05 11:57:38 +0100

    Update userdb.php

--- include/userdb.php
+++ include/userdb.php
@@ -1,12 +1,12 @@
 <?php
 /* user / client database */
 $userdb_array = [
-    'CN8VX-SV'		=> 'Répéteur VHF de Mohemadia 145.250Mhz Simplex',
+	'CN8VX-SV'		=> 'Répéteur VHF de Mohemadia 145.250Mhz Simplex',
 	'CN8VX-T'		=> 'Répéteur de CN8VX en test',
-    'GW_SVX-MA'		=> 'Gateway Svx to DMR',
-    'CN8EAA-L'		=> 'Répéteur VHF de Témara 145.73750Mhz Duplex',
-    'CN8MC-RU'		=> 'Répéteur UHF de RABAT 439.850Mhz Duplex', 
-    'CN8MSF-SU'		=> 'Répéteur UHF de Khouribga 433.600Mhz Simplex'
+	'GW_SVX-MA'		=> 'Gateway Svx to DMR',
+	'CN8EAA-L'		=> 'Répéteur VHF de Témara 145.73750Mhz Duplex',
+	'CN8MC-RU'		=> 'Répéteur UHF de RABAT 439.850Mhz Duplex', 
+	'CN8MSF-SU'		=> 'Répéteur UHF de Khouribga 433.600Mhz Simplex'
     
 ];
 
