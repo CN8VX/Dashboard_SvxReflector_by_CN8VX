4d0ea02362911a3304246b6ea97acd89006fc10b
Youness CN8VX <cn8vx.ma@gmail.com>
2024-10-05 11:55:18 +0100

    Update tgdb.php

--- include/tgdb.php
+++ include/tgdb.php
@@ -1,10 +1,9 @@
 <?php
-/* talkgroup / number alias database */
+//'#talkgroup' => 'number alias database'
 $tgdb_array = [
-    '#0'     => 'IDLE',
-    '#6041'    => 'Cross Mode DMR = ANALOGIQUE',
+	'#0'     => 'IDLE',
+	'#6041'    => 'Cross Mode DMR = ANALOGIQUE',
 	'#604' => 'Morocco - National',
 	'#604112' => 'EmComm Morocco'
 ];
-
 ?>
