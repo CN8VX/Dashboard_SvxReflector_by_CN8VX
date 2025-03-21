5f5abfe56ca482ae1c6e6eb51e3cf13ea742314f
Youness CN8VX <cn8vx.ma@gmail.com>
2025-03-21 00:34:31 +0000

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
 <footer>
 <br><br>
     <div class="fixed-footer">
-        <p>SVXReflector Dashboard by CN8VX, based at svxrdb-server. Rewrite and Designed by <a class="lien" target="_blank" href="mailto:cn8vx.ma@gmail.com">CN8VX</a> Youness sysop of <a target="_blank" href="https://www.dmr-maroc.com/">Serveur DMR-MAROC</a>. Version <b>4.2</b> © <?php echo "2023 - " . date("Y"); ?> All rights reserved
+        <p>SVXReflector Dashboard by CN8VX, based at <a target="_blank" href="https://github.com/CN8VX/Dashboard_SvxReflector_by_CN8VX">svxrdb-server</a>. Rewrite and Designed by <a class="lien" target="_blank" href="mailto:cn8vx.ma@gmail.com">CN8VX</a> Youness sysop of <a target="_blank" href="https://www.dmr-maroc.com/">Serveur DMR-MAROC</a>. Version <b>4.2</b> © <?php echo "2023 - " . date("Y"); ?> All rights reserved</p>
     </div>
 </footer>
